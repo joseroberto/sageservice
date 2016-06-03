d37da44029b1f7d6baefb25803eccace894711bc
Jose Roberto <joserobertovasconcelos@gmail.com>
2016-06-03 09:59:56 -0300

    Testes

--- test/index.js
+++ test/index.js
@@ -3,22 +3,57 @@ var boot = require('../bin/www').boot,
     port = require('../bin/www').port,
     superagent = require('superagent'),
     expect = require('expect.js');
-    
-    describe('server', function () {
-        before(function () {
-            boot();
-        });
-        describe('homepage', function(){
-        it('should respond to GET',function(done){
+
+var uri = 'http://localhost:'+port;
+var api = uri + '/api/v1/';
+
+describe('sage service', function () {
+    before(function () {
+        boot();
+    });
+    /**
+     * Rotas
+     */
+    describe('servidor', function(){
+        it('está on line',function(done){
             superagent
-            .get('http://localhost:'+port)
+            .get(uri)
             .end(function(err, res){
                 expect(res.status).to.equal(200);
                 done()
-                })
+             })
+        })
+    });
+
+    /**
+     * Metas
+     */
+    describe('meta', function(){
+        it('lista metas',function(done){
+            superagent
+            .get(api + 'meta') 
+            .end(function(err, res){
+                expect(res.status).to.equal(200);
+                done()
             })
-        });
-        after(function () {
-            shutdown();
-        });
+        })
+    });        
+
+    /**
+     * Indicadores
+     */
+    describe('indicador', function(){
+        it('lista indicadores',function(done){
+            superagent
+            .get(api + 'indicador') 
+            .end(function(err, res){
+                expect(res.status).to.equal(200);
+                done()
+            })
+        })
+    });        
+
+    after(function () {
+        shutdown();
+    });
   });

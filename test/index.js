2570de15fcfd9254f97859c565a9f3ed73bbf99b
Jose Roberto <jose.roberto@dinamonetworks.com>
2016-06-03 00:16:18 -0300

    Teste de indicadores

--- test/index.js
+++ test/index.js
@@ -2,23 +2,90 @@ var boot = require('../bin/www').boot,
     shutdown = require('../bin/www').shutdown,
     port = require('../bin/www').port,
     superagent = require('superagent'),
-    expect = require('expect.js');
+    expect = require('expect.js'),
+    async = require('async');
     
-    describe('server', function () {
+var uri = 'http://localhost:'+port;
+var api = uri + '/api/v1';
+
+describe('sageservice', function () {
         before(function () {
+            console.log('boot');
             boot();
         });
-        describe('homepage', function(){
-        it('should respond to GET',function(done){
-            superagent
-            .get('http://localhost:'+port)
-            .end(function(err, res){
-                expect(res.status).to.equal(200);
-                done()
-                })
-            })
+        
+        describe('Pagina inicial', function(){
+            it('responde ao GET',function(done){
+                superagent
+                .get(uri)
+                .end(function(err, res){
+                    expect(res.status).to.equal(200);
+                    done();
+                    })
+            }),
+            it('retorna dados de indicadores',function(done){
+                superagent
+                .get(api + '/indicador')
+                .end(function(err, res){
+                    expect(res.status).to.equal(200);
+                    expect(res.body).to.be.an('array');
+                    expect(res.body.length).to.be.above(0);
+                    done()
+                    })
+            }),
+            it('retorna dados de metas',function(done){
+                superagent
+                .get(api + '/meta')
+                .end(function(err, res){
+                    expect(res.status).to.equal(200);
+                    expect(res.body).to.be.an('array');
+                    expect(res.body.length).to.be.above(0);
+                    done();
+                    })
+            }) //,
+        
+    /////////////
+    /*
+    it('retorna dados de metas',function(done){
+        superagent
+        .get(api + '/indicador')
+        .end(function(err, res){
+            
+            describe('Teste indicadores', function(){
+            ////////////////
+            async.eachSeries(res.body,(item, callback) =>{
+                it('teste do indicador - ' + item.codigo,function(doneindicador){
+                    //superagent
+                    //.get(api + '/indicador/'+item.codigo)
+                    //.end(function(err, res){
+                    //    if(err) {
+                    //        expect(err).to.be(undefined);
+                    //        return;
+                    //    }
+                    //    expect(res.status).to.equal(200);
+                    //    expect(res.body).to.be.an('array');
+                    //    expect(res.body.length).to.be.above(0);
+                    //    async.setImmediate(function () {
+                            console.log(item.codigo);
+                            callback(null,done);
+                            doneindicador();
+                    //    });
+                    }); // it
+                }, (done)=>{
+                    done();
+                }); // async
+            ///////////////////////
+            });
+            //done();
         });
-        after(function () {
-            shutdown();
-        });
-  });
+  });    
+  //////////
+  */
+
+     });
+     after(function () {
+         console.log('shutdown');
+         shutdown();
+     });      
+});       
+ 

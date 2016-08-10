3b423b7b24a84fa7feaf0e0be2bb16c2546586f0
Jose Roberto <joserobertovasconcelos@gmail.com>
2016-08-10 09:43:23 -0300

    Versao com grafico

--- routes/index.js
+++ routes/index.js
@@ -18,5 +18,12 @@ router.get('/login', function(req, res, next) {
   res.render('login');
 });
 
+router.get('/teste', (req,res,next) =>{
+    res.render('teste');
+});
+
+router.get('/teste2', (req,res,next) =>{
+    res.render('teste2',{ qtdMetas:20, qtdIndicadores: 7,  percMetas:15, percIndicadores: 10});
+});
 
 module.exports = router;
